366a53ff594c241306e96895f691a7d418500e35
huzaifa3162 <huzaifasiddiqui730@gmail.com>
2025-01-01 17:42:37 +0500

    fix: vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ const app = express();
 const port = 5000;
 app.use(cors());
 
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+res.json({message: "Backend is working"})
+})
+
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/note", require("./routes/note"));
 app.listen(port, () => {
